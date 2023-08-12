0841ca7a633e62e71f35cb4effa8cddd878a0fbc
VolNa <grizli887@mail.ru>
2023-08-12 08:59:57 +0300

    fix: fixed exports in registrationt page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,4 +2,4 @@ export * from './MainPage';
 export * from './AboutPage';
 export * from './NotFoundPage';
 export * from './LoginPage';
-export * from './RegistrationPage';
+export * from './RegistrationtPage';
