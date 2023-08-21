6b89a04ba17ec977cc68424a721288171c78dd46
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-08-21 20:04:29 +0300

    feat: add loginBtn and index.tsx tests

--- src/components/LoginBtn/LoginBtn.test.tsx
+++ src/components/LoginBtn/LoginBtn.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import LoginBtn from './LoginBtn';
@@ -15,5 +15,25 @@ describe('LoginBtn', () => {
       </Provider>
     );
     expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  test('opens and closes the menu on button click', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <LoginBtn />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const button = screen.getByTestId('loginBtnIcon');
+
+    fireEvent.click(button);
+    const menuLogin = screen.getByTestId('menuLogin');
+    expect(menuLogin).toBeInTheDocument();
+
+    fireEvent.click(button);
+    const menuRegister = screen.getByTestId('menuRegister');
+    expect(menuRegister).toBeInTheDocument();
   });
 });
