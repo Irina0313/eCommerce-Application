a56b9119502271edd0922d5255418af7e49b31d6
Nikolay Tyushkov <begemot@begemotov.net>
2023-08-26 07:32:19 +0300

    fix: fix code style

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material';
 
 export function ProfilePage() {
   return (
-    <Typography variant="h1" m={10}>
+    <Typography variant='h1' m={10}>
       Profile Page
     </Typography>
   );
