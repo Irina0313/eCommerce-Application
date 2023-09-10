d32dfcc831b6519f6c26be975c41808807c9b152
Nikolay Tyushkov <begemot@begemotov.net>
2023-09-10 20:06:54 +0300

    feat: add Empty Cart Message and Link to Catalog

--- src/pages/BasketPage/BasketPage.tsx
+++ src/pages/BasketPage/BasketPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box, CircularProgress, Typography, Button } from '@mui/material';
 import { Container } from '@mui/system';
+import { Link } from 'react-router-dom';
 
 export function BasketPage() {
   const [list, setList] = useState<string[]>([]);
@@ -11,7 +12,8 @@ export function BasketPage() {
     setLoading(true);
     new Promise<string[]>((resolve) => {
       //   throw Error('test error');
-      setTimeout(() => resolve(['1', '2', '3']), 1000);
+      // setTimeout(() => resolve(['1', '2', '3']), 1000);
+      setTimeout(() => resolve([]), 1000);
     })
       .then((body: string[]) => {
         setLoading(false);
@@ -26,8 +28,8 @@ export function BasketPage() {
   }, []);
 
   return (
-    <Container maxWidth='xl' sx={{ display: 'flex' }}>
-      <Typography>comming soon</Typography>
+    <Container maxWidth='xl' sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Typography variant='h1'>Busket page</Typography>
 
       {loading && (
         <Box sx={{ display: 'flex', justifyContent: 'center', mt: 3 }}>
@@ -41,7 +43,23 @@ export function BasketPage() {
         </Typography>
       )}
 
-      {!loading && !error && list.length > 0 && <Typography> : {list.join()}</Typography>}
+      {!loading && !error ? (
+        list.length > 0 ? (
+          <Typography> : {list.join()}</Typography>
+        ) : (
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 3 }}>
+            <Typography variant='h3' sx={{ margin: '3rem 0 0 0' }}>
+              Sorry freind:(
+            </Typography>
+            <Typography variant='h5' component='p' sx={{ margin: '3rem 0 2rem' }}>
+              It looks your basket is empty, but you can still choose something delightful....
+            </Typography>
+            <Link to={'/catalog'}>
+              <Button variant='contained'>see our catalog</Button>
+            </Link>
+          </Box>
+        )
+      ) : null}
     </Container>
   );
 }
