601dd10fffff6c6c637ec818504963ce754671c5
VolNa <grizli887@mail.ru>
2023-09-10 15:36:20 +0300

    feat: add add to cart btn and img carousel to prod card

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -5,7 +5,6 @@ import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
 import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Carousel } from 'react-responsive-carousel';
 import { returnProductByKey } from '../../api/Product';
 import { ProductData } from '@commercetools/platform-sdk';
 import { useAppSelector } from '../../hooks/useAppSelector';
@@ -14,9 +13,10 @@ import { setProd } from '../../store/productSlice';
 import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import './style.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import GoHomeBth from '../../components/GoHomeBtn/GoHomeBth';
+import { ImgCarousel } from '../../components/UI-components/ImgCarousel/ImgCarousel';
+import AddToCartBtn from '../../components/UI-components/AddToCartBtn/AddToCartBtn';
 
 export function ProductPage() {
   const prodTemplate = useAppSelector((state) => state.productReducer);
@@ -54,6 +54,13 @@ export function ProductPage() {
     });
     return arr;
   };
+  const imageUrlsArr = () => {
+    const arr: Array<string> = [];
+    prodData.masterVariant.images?.forEach((image) => {
+      arr.push(image.url);
+    });
+    return arr;
+  };
 
   return (
     <Grid container spacing={2} justifyContent={'center'} data-testid={'catalog'}>
@@ -76,34 +83,30 @@ export function ProductPage() {
                 </Typography>
               </Grid>
 
-              <Grid item md={6} xs={12} sx={{ height: '60vh', justifyContent: 'center', justifyItems: 'center', position: 'relative', marginBottom: '10vh' }}>
-                <ZoomInIcon sx={{ position: 'absolute', top: '0', right: '15%' }} onClick={() => setOpen(true)} />
-                <Carousel data-testid='carousel' showArrows={false} dynamicHeight={false} showStatus={false}>
-                  {prodData.masterVariant.images?.map((image) => (
-                    <div key={image.url}>
-                      <img src={image.url}></img>
-                    </div>
-                  ))}
-                </Carousel>
-                <Lightbox
-                  open={open}
-                  close={() => setOpen(false)}
-                  carousel={{ preload: 3 }}
-                  render={
-                    prodData.masterVariant.images
-                      ? prodData.masterVariant.images.length <= 1
-                        ? {
-                            buttonPrev: () => null,
-                            buttonNext: () => null,
-                          }
+              <ImgCarousel
+                imgUrls={imageUrlsArr()}
+                zoomIcon={<ZoomInIcon sx={{ position: 'absolute', top: '0', right: '15%' }} onClick={() => setOpen(true)} />}
+                lightBox={
+                  <Lightbox
+                    open={open}
+                    close={() => setOpen(false)}
+                    carousel={{ preload: 3 }}
+                    render={
+                      prodData.masterVariant.images
+                        ? prodData.masterVariant.images.length <= 1
+                          ? {
+                              buttonPrev: () => null,
+                              buttonNext: () => null,
+                            }
+                          : undefined
                         : undefined
-                      : undefined
-                  }
-                  slides={imageSrcArr()}
-                  plugins={[Zoom, Thumbnails]}
-                  data-testid='lightBox'
-                />
-              </Grid>
+                    }
+                    slides={imageSrcArr()}
+                    plugins={[Zoom, Thumbnails]}
+                    data-testid='lightBox'
+                  />
+                }
+              ></ImgCarousel>
 
               <Grid item md={6} xs={12}>
                 <Grid item xs={12} sx={{ height: 'max-content' }} data-testid='prices'>
@@ -164,17 +167,7 @@ export function ProductPage() {
                         />
                       </Grid>
                       <Grid item xs={10}>
-                        {prodData.masterVariant.key === '...123abc' ? (
-                          <Button onClick={() => console.log(amount)} disabled variant='contained' sx={{ margin: ' 1rem 0' }} data-testid='addToCart'>
-                            <AddShoppingCartIcon></AddShoppingCartIcon>
-                            Add to cart
-                          </Button>
-                        ) : (
-                          <Button onClick={() => console.log(amount)} variant='contained' sx={{ margin: ' 1rem 0' }} data-testid='addToCart'>
-                            <AddShoppingCartIcon></AddShoppingCartIcon>
-                            Add to cart
-                          </Button>
-                        )}
+                        {prodData.masterVariant.key === '...123abc' ? <AddToCartBtn handleClick={() => console.log(amount)} disabled={true} /> : <AddToCartBtn handleClick={() => console.log(amount)} />}
                       </Grid>
                     </Grid>
                   </>
