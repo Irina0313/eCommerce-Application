855653293de7e2a62a06ff0c4432078c371d769b
VolNa <grizli887@mail.ru>
2023-09-18 19:26:31 +0300

    feat: add fixed header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -35,7 +35,7 @@ export function Header() {
   }));
 
   return (
-    <AppBar position='static' data-testid={'header'}>
+    <AppBar position='fixed' data-testid={'header'}>
       <Container maxWidth='md'>
         <Toolbar disableGutters>
           <EmojiEmotionsOutlinedIcon fontSize='large' sx={{ color: 'wheat' }} />
